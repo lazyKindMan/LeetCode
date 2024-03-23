c89e391cadd597cf6d86e3c58e5c7066daa37a3b
GitHub Action <action@github.com>
2024-03-23 16:17:49 +0000

    [20240324] Add daily LeetCode problem

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import unittest
 from importlib.util import spec_from_file_location, module_from_spec
 
 # Question ID that wants to test, modify here as passing arguments
-QUESTIONS = ['209', '228']
+QUESTIONS = ['228', '530']
 
 
 class Test(unittest.TestCase):
