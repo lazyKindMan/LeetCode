e5be8eb138a6f369352d22668506783812850764
GitHub Action <action@github.com>
2024-03-24 16:18:08 +0000

    [20240325] Add daily LeetCode problem

---
+++ problems/518/solution.py
@@ -0,0 +1,9 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        pass
+
+    def change(self, amount: int, coins: List[int]) -> int:
