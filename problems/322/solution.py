bd57919c4541dba224eebfedce7856f7c8dcd618
longsq <longsq@1575032914718481>
2024-03-25 11:32:07 +0800

    make up a missed punch by submit 322 problems

--- problems/322/solution.py
+++ problems/322/solution.py
@@ -4,6 +4,13 @@ from typing import *
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        pass
+        return self.coinChange(test_input[0], test_input[1])
 
     def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [-1] * (amount + 1)
+        dp[0] = 0
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                if dp[i - coin] != -1:
+                    dp[i] = min(dp[i], dp[i - coin] + 1) if dp[i] != -1 else dp[i - coin] + 1
+        return dp[amount]
