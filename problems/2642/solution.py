ac7411b460fccc9376c381c929b66a9b387a32de
GitHub Action <action@github.com>
2024-03-25 16:19:53 +0000

    [20240326] Add daily LeetCode problem

---
+++ problems/2642/solution.py
@@ -0,0 +1,25 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        pass
+
+
+class Graph:
+
+    def __init__(self, n: int, edges: List[List[int]]):
+
+
+    def addEdge(self, edge: List[int]) -> None:
+
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+
+
+
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
