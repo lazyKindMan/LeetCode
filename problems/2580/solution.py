66babaabdffe63537a1c4549ae59522de2e1d5f2
GitHub Action <action@github.com>
2024-03-26 16:20:05 +0000

    [20240327] Add daily LeetCode problem

---
+++ problems/2580/solution.py
@@ -0,0 +1,9 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        pass
+
+    def countWays(self, ranges: List[List[int]]) -> int:
