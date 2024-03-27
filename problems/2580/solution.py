3da8b003e45cbb3c4a5c41f21a0585e347ac81d1
longsq <longsq@1575032914718481>
2024-03-27 09:31:04 +0800

    add everyday problem solution

--- problems/2580/solution.py
+++ problems/2580/solution.py
@@ -4,6 +4,16 @@ from typing import *
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        pass
+        return self.countWays(test_input)
 
     def countWays(self, ranges: List[List[int]]) -> int:
+        # Especially when the goal is to solve a counting problem, it is not necessary to consider the specific details; just focus on the counting aspect
+        ranges.sort()
+        cnt = 0
+        right = -1
+        for st, ed in ranges:
+            if st > right:
+                cnt += 1
+            right = max(ed, right)
+        mod = 10 ** 9 + 7
+        return pow(2, cnt, mod)
