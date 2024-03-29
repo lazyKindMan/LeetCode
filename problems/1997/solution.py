2cf60450b325bbd43f779db1da404708f41a0c4d
longsq <longsq@1575032914718481>
2024-03-29 11:24:12 +0800

    add everyday problem solution

--- problems/1997/solution.py
+++ problems/1997/solution.py
@@ -1,9 +1,22 @@
 import solution
 from typing import *
 
-
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        pass
+        return self.firstDayBeenInAllRooms(test_input)
 
     def firstDayBeenInAllRooms(self, nextVisit: List[int]) -> int:
+        n = len(nextVisit)
+        dp = [0] * n
+        dp[0] = 1
+        ans = 0
+        mod = 10 ** 9 + 7
+        for e in nextVisit[1:]:
+            if dp[e] > 0:
+                dp[e] += 1
+            else:
+                dp[e] = 1
+                dp[e - 1] += 1
+        for d in dp:
+            ans = (ans + d) % mod
+        return ans
--- problems/2908/solution.py
+++ problems/2908/solution.py
@@ -4,6 +4,29 @@ from typing import *
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        pass
+        return self.minimumSum(test_input)
 
     def minimumSum(self, nums: List[int]) -> int:
+        # O(n^2)
+        ans, n = -1, len(nums)
+        # min_i = nums[0]
+        # for j in range(1, n - 1):
+        #     if min_i < nums[j]:
+        #         for k in range(j + 1, n):
+        #             if nums[k] < nums[j]:
+        #                 temp = nums[k] + nums[j] + min_i
+        #                 ans = temp if ans == -1 or temp < ans else ans
+        #     min_i = min(min_i, nums[j])
+        #
+        # Maintain a minimal suffix, and then scan from front to back. Time complexity O(n)
+        suf = [0] * (n - 1) + [nums[n - 1]]
+        min_i = nums[0]
+        for k in range(n - 2, 0, -1):
+            suf[k] = min(nums[k], suf[k + 1])
+        for j in range(1, n - 1):
+            if min_i < nums[j] and nums[j] > suf[j]:
+                temp = nums[j] + suf[j] + min_i
+                ans = temp if ans == -1 or temp < ans else ans
+            min_i = min(min_i, nums[j])
+        return ans
+
