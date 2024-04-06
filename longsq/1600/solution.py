01bbf7cb7696852f5ff9292834bde4f6b99b242b
GitHub Action <action@github.com>
2024-04-06 16:20:13 +0000

    [20240407] Add daily LeetCode problem

---
+++ longsq/1600/solution.py
@@ -0,0 +1,35 @@
+import solution
+from typing import *
+from object_libs import call_method
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        ops, inputs = test_input
+        obj = ThroneInheritance(*inputs[0])
+        return [None] + [call_method(obj, op, *ipt) for op, ipt in zip(ops[1:], inputs[1:])]
+
+class ThroneInheritance:
+
+    def __init__(self, kingName: str):
+        pass
+
+
+    def birth(self, parentName: str, childName: str) -> None:
+            pass
+
+
+    def death(self, name: str) -> None:
+            pass
+
+
+    def getInheritanceOrder(self) -> List[str]:
+        pass
+
+
+
+# Your ThroneInheritance object will be instantiated and called as such:
+# obj = ThroneInheritance(kingName)
+# obj.birth(parentName,childName)
+# obj.death(name)
+# param_3 = obj.getInheritanceOrder()
