6eeaa715eb30f66d632ea3c009399346dade50ed
longsq <longsq@1575032914718481>
2024-04-09 10:25:11 +0800

    commit daily solution at 2529

--- longsq/2529/solution.py
+++ longsq/2529/solution.py
@@ -7,4 +7,17 @@ class Solution(solution.Solution):
         return self.maximumCount(test_input)
 
     def maximumCount(self, nums: List[int]) -> int:
-            pass
+        n = len(nums)
+        def find_zero(target: int) -> int:
+            l, r = 0, n -1
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] >= target:
+                    r = mid - 1
+                elif nums[mid] < target:
+                    l = mid + 1
+            return l
+        pos = n - find_zero(1)
+        neg = find_zero(0)
+        print(find_zero(1), neg)
+        return max(neg, pos)
--- longsq/2529/testcase.py
+++ longsq/2529/testcase.py
@@ -7,9 +7,9 @@ case = namedtuple("Testcase", ["Input", "Output"])
 class Testcase(testcase.Testcase):
 	def __init__(self):
 		self.testcases = []
-		self.testcases.append(case(Input=[-2, -1, -1, 1, 2, 3], Output=3))
-		self.testcases.append(case(Input=[-3, -2, -1, 0, 0, 1, 2], Output=3))
-		self.testcases.append(case(Input=[5, 20, 66, 1314], Output=4))
+		# self.testcases.append(case(Input=[-2, -1, -1, 1, 2, 3], Output=3))
+		self.testcases.append(case(Input=[0, 0, 0, 0, 0, 0, 0], Output=3))
+		# self.testcases.append(case(Input=[5, 20, 66, 1314], Output=4))
 
 	def get_testcases(self):
 		return self.testcases
