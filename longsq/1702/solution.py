bc1730cdf89d77371d6c6013500d310717be574f
longsq <longsq@1575032914718481>
2024-04-10 10:46:26 +0800

    commit daily solution

--- longsq/1702/solution.py
+++ longsq/1702/solution.py
@@ -7,4 +7,20 @@ class Solution(solution.Solution):
         return self.maximumBinaryString(test_input)
 
     def maximumBinaryString(self, binary: str) -> str:
-            pass
+        n = len(binary)
+        last_zero_index = -1
+        binary_list = list(binary)
+        for i in range(n - 1):
+            if binary_list[i] == '0':
+                if binary_list[i + 1] == '0':
+                    binary_list[i] = '1'
+                    last_zero_index = i + 1
+                else:
+                    last_zero_index = i
+            else:
+                if binary_list[i + 1] == '0' and last_zero_index != -1:
+                    binary_list[i + 1] = '1'
+                    binary_list[last_zero_index] = '1'
+                    binary_list[last_zero_index + 1] = '0'
+                    last_zero_index += 1
+        return ''.join(binary_list)
