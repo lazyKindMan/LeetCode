ae7bea8aa9f4363e8b5838327f9d217d0fd9507b
GitHub Action <action@github.com>
2024-04-12 14:09:44 +0000

    [20240412] Add daily problem solution

--- longsq/2923/solution.py
+++ longsq/2923/solution.py
@@ -7,4 +7,8 @@ class Solution(solution.Solution):
         return self.findChampion(test_input)
 
     def findChampion(self, grid: List[List[int]]) -> int:
-            pass
+        maxChampion = 0
+        for i in range(1, len(grid)):
+            if grid[i][maxChampion] == 1:
+                maxChampion = i
+        return maxChampion
