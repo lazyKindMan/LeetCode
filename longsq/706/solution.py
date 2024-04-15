7ea96e858ccb776a09e573da0b19cc6287447609
longsq <longsq@1575032914718481>
2024-04-15 09:47:58 +0800

    commit daily solution 706

--- longsq/706/solution.py
+++ longsq/706/solution.py
@@ -9,24 +9,28 @@ class Solution(solution.Solution):
         obj = MyHashMap()
         return [None] + [call_method(obj, op, *ipt) for op, ipt in zip(ops[1:], inputs[1:])]
 
+
 class MyHashMap:
 
     def __init__(self):
-        pass
-
+        self.base = 768
+        self.data = [dict()] * self.base
 
     def put(self, key: int, value: int) -> None:
-            pass
-
+        hash_key = key % self.base
+        val_list = self.data[hash_key]
+        val_list[key] = value
 
     def get(self, key: int) -> int:
-            pass
-
+        hash_key = key % self.base
+        val_list = self.data[hash_key]
+        return val_list[key] if key in val_list else -1
 
     def remove(self, key: int) -> None:
-            pass
-
-
+        hash_key = key % self.base
+        val_list = self.data[hash_key]
+        if key in val_list:
+            val_list.pop(key)
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
