6af936ac116544e3c4f93043998d35e0f45cb6a7
GitHub Action <action@github.com>
2024-07-10 02:03:46 +0000

    [20240710] Add daily LeetCode problem

---
+++ longsq/longsq_228/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.summaryRanges(test_input)
+
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        pass
+
