4ea5026720d483ff8c17f0bf0b193fc60881b331
longsq <longsq@1575032914718481>
2024-07-29 18:00:33 +0800

    commit solution

--- qubhjava/test/ProblemsTest.java
+++ qubhjava/test/ProblemsTest.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class ProblemsTest {
 
     private static final Logger log = LoggerFactory.getLogger(ProblemsTest.class);
-	private static final String[][] PROBLEMS = {{"19", "longsq"}};
+	private static final String[][] PROBLEMS = {{"236", "longsq"}};
 
     @TestFactory
     @SuppressWarnings("unchecked")
